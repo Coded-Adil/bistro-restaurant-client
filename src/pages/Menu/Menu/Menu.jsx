04c9bf67dde9c78eeba92859911fe3f052907cb4
Adil Rahman <itsadil2022@gmail.com>
2024-12-31 14:08:10 +0600

    Added parallar

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,17 @@
 import { Helmet } from 'react-helmet-async';
-
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/menu-bg.jpg'
+import PopulerMenu from '../../Home/PopularMenu/PopulerMenu';
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
+            <Cover img={menuImg} title="Our Menu" />
+            <PopulerMenu />
+            <PopulerMenu />
+            <PopulerMenu />
         </div>
     );
 };
