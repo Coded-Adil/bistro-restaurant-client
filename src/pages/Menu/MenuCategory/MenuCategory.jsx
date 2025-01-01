64b1dd3492107cbf8e3d1f0cfd172338128c57d9
Adil Rahman <itsadil2022@gmail.com>
2025-01-01 13:52:07 +0600

    Added all the menu items

--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,8 +1,15 @@
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = () => {
+const MenuCategory = ({ items, title, coverImg }) => {
     return (
-        <div>
-            
+        <div className="py-8">
+            {title && <Cover img={coverImg} title={title} />}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 my-16">
+                {
+                    items.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+                }
+            </div>
         </div>
     );
 };
