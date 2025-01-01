9e1847d074a878325195d13a42ca9c0b41a04488
Adil Rahman <itsadil2022@gmail.com>
2025-01-01 13:00:31 +0600

    made a custom hook for menu

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
-import PopulerMenu from '../../Home/PopularMenu/PopulerMenu';
 const Menu = () => {
     return (
         <div>
@@ -9,9 +8,6 @@ const Menu = () => {
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={menuImg} title="Our Menu" />
-            <PopulerMenu />
-            <Cover img={menuImg} title="Our Menu" />
-            <PopulerMenu />
         </div>
     );
 };
