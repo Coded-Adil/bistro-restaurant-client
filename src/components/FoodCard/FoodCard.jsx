9026b52e40c653e9144b8b43175a0219039399cb
Adil Rahman <itsadil2022@gmail.com>
2025-01-17 20:18:23 +0600

    Added user login or not func before add to cart

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,9 +1,31 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
-const FoodCard = ({item}) => {
+const FoodCard = ({ item }) => {
     const { name, recipe, price, image } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
 
     const handleAddToCart = food => {
-        console.log(food);
+        if (user && user.email) {
+            // Add to Cart
+        }
+        else {
+            Swal.fire({
+                title: "You Are Not Logged In",
+                text: "Please LogIn before Adding Item to the Cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Login"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login');
+                }
+            });
+        }
     }
 
     return (
@@ -13,7 +35,7 @@ const FoodCard = ({item}) => {
                     src={image}
                     alt={name} />
             </figure>
-                <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4">${price}</p>
+            <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4">${price}</p>
             <div className="card-body">
                 <h2 className="card-title text-center">{name}</h2>
                 <p className="text-center">{recipe}</p>
