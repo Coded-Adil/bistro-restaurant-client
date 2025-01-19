c3b95dca2aebe6935ca6d090dae555538d550670
Adil Rahman <itsadil2022@gmail.com>
2025-01-19 21:25:08 +0600

    Added useCart custom hook

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -2,9 +2,11 @@ import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 import { FaShoppingCart } from "react-icons/fa";
+import useCart from "../../../hooks/useCart";
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
+    const [cart] = useCart();
 
     const handleLogOut = () => {
         logOut()
@@ -21,7 +23,7 @@ const Navbar = () => {
             <li><Link to={'/'}>
                 <button className="btn">
                     <FaShoppingCart className="mr-2" />
-                    <div className="badge badge-secondary">+0</div>
+                    <div className="badge badge-secondary">+{cart.length}</div>
                 </button>
             </Link></li>
             {
